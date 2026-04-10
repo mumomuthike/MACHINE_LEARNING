76dcb1ec2b6758c1e65f0600171aeeed69b270db
mumomuthike <mumo.muthike@gmail.com>
2026-04-10 17:27:51 -0400

    intializing

---
+++ numpyvis.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Load the array
+data = np.load('/Users/andrewyi/Documents/Machine Learning Final Project/models/reconstruction_errors.npy')
+
+# View the contents
+print(data)
